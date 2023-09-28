b8edf7c4120c933aadbe2bb3eeab71c9f8121563
AbhiGone <giriabhinaya@gmail.com>
2023-09-28 09:37:07 -0400

    compare

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -1,5 +1,33 @@
-public class Main {
+import java.util.Scanner;
+
+public class NumCompare {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int numOne;
+        int numTwo;
+
+        System.out.println("enter two numbers");
+        if(scan.hasNextInt()){
+            numTwo = scan.nextInt();
+        }else{
+            System.out.println("Error: You have entered an invalid integer.");
+            return;
+        }
+
+        if(scan.hasNextInt()){
+            numOne = scan.nextInt();
+        }else{
+            System.out.println("Error: You have entered an invalid integer.");
+            return;
+        }
+
+        if(numOne > numTwo){
+            System.out.println(numOne + " is greater than " + numTwo);
+        }else if(numOne < numTwo){
+            System.out.println(numOne + " is less than " + numTwo);
+        }else{
+            System.out.println(numOne + " is equal to " + numTwo);
+        }
+
     }
 }
